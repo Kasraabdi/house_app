e298f466ca970ac6972030d98b7bfd6cc1ca8e5b
kasra <kasraabdi2024@gmail.com>
2025-05-22 18:53:38 +0330

    TOZIHAT KHONE

---
+++ house_info.py
@@ -0,0 +1,102 @@
+from tkinter import *
+import tkinter.ttk as ttk
+import tkinter.messagebox as msg
+from file_manager import *
+from validator import *
+
+house_list = read_from_file("house.dat")
+
+
+def load_data(house_list):
+    house_list = read_from_file("house.dat")
+    for row in table.get_children():
+        table.delete(row)
+
+    for house in house_list:
+        table.insert("", END, values=(house .id , house .m ,house.area , house.address))
+
+
+def reset_form():
+    id.set(len(house_list) + 1)
+    m.set("")
+    area.set("")
+    address.set(0)
+    load_data(house_list)
+
+
+def save_btn_click():
+     house= (id.get(), m.get(), area.get(), address.get())
+    errors = house_validator(house)
+    if errors:
+        msg.showerror("Errors", "\n".join(errors))
+    else:
+        msg.showinfo("Saved", "house saved")
+        house_list.append(house)
+        write_to_file("houses.dat", house_list)
+        reset_form()
+
+
+def table_select(x):
+    selected_house = table.item(table.focus())["values"]
+    if selected_house:
+        id.set(selected_house[0])
+        m.set(selected_house[1])
+        area.set(selected_house[2])
+        address.set(selected_house[3])
+
+
+def edit_btn_click():
+    pass
+
+
+def remove_btn_click():
+    pass
+
+
+window = Tk()
+window.title("house Info")
+window.geometry("610x270")
+
+# Id
+Label(window, text="Id").place(x=20, y=20)
+id = IntVar(value=1)
+Entry(window, textvariable=id, state="readonly").place(x=80, y=20)
+
+# M
+Label(window, text="M").place(x=20, y=60)
+m = IntVar()
+Entry(window, textvariable=m).place(x=80, y=60)
+
+# area
+Label(window, text="area").place(x=20, y=100)
+area = int()
+Entry(window, textvariable=area).place(x=80, y=100)
+
+# address
+Label(window, text="address").place(x=20, y=140)
+address = IntVar()
+Entry(window, textvariable=address).place(x=80, y=140)
+
+table = ttk.Treeview(window, columns=[1, 2, 3, 4], show="headings")
+table.heading(1, text="Id")
+table.heading(2, text="M")
+table.heading(3, text="area")
+table.heading(4, text="address")
+
+table.column(1, width=60)
+table.column(2, width=100)
+table.column(3, width=100)
+table.column(4, width=100)
+
+table.bind("<<TreeviewSelect>>", table_select)
+
+table.place(x=230, y=20)
+
+Button(window, text="Save", width=6, command=save_btn_click).place(x=20, y=220)
+Button(window, text="Edit", width=6, command=edit_btn_click).place(x=90, y=220)
+Button(window, text="Remove", width=6, command=remove_btn_click).place(x=160, y=220)
+Button(window, text="Clear", width=6, command=reset_form).place(x=20, y=180, width=190)
+
+reset_form()
+
+window.mainloop()
---
+++ validator.py
@@ -0,0 +1,21 @@
+import re
+def house_validator(house):
+    errors = []
+    if not (type(house[0]) == int and house[0]>0):
+        errors.append('house ID must be an integer > 0')
+
+    if not (type(house[1]) == int and re.match(r"^[0-9\s]{4}$", house[1])):
+        errors.append('House M is Invalid')
+
+
+    if not (type(house[2]) == int and re.match(r"^[0-9\s]{2}$", house[2])):
+        errors.append('House area is Invalid')
+
+    if not (type(house[3]) == str and house[3]>0):
+        errors.append('House address must be an string > 0')
+
+    if not (type(house[4]) == str and house[4]>0):
+        errors.append('House option must be string > 0')
+
+    return errors
+
